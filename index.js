6accb1103552a3892847c9c1836a1ca65b04ac0d
storngfornt <au0038173@gmail.com>
2024-08-16 22:30:32 +0600

    pagination skip limit added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
@@ -10,8 +10,6 @@ app.use(express.json());
 
 //mongo start =====================
 
-
-
 const uri = `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASS}@cluster0.v2tnkbl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -20,28 +18,74 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    const db = client.db('productsDB')
-    const productsCollection = db.collection("products")
+    const db = client.db("productsDB");
+    const productsCollection = db.collection("products");
 
-    app.get("/products",async(req,res) => {
-        const result = await productsCollection.find().toArray() 
-        res.send(result)
-    })
-    app.get("/productsCount",async(req,res) => {
-        const totalProduct = await productsCollection.countDocuments()
-        res.send({totalProduct})
-    })
-    
+    app.get("/products", async (req, res) => {
+      const { name, category, brand } = req.query || "";
+      let query = {};
+
+      if (name || category || brand) {
+        query = {
+          $or: [],
+        };
+
+        if (name) {
+          query.$or.push({ name: { $regex: name, $options: "i" } });
+        }
+        if (category) {
+          query.$or.push({ category: { $regex: category, $options: "i" } });
+        }
+        if (brand) {
+          query.$or.push({ brand: { $regex: brand, $options: "i" } });
+        }
+      }
+
+      const size = parseInt(req.query.size);
+      const page = parseInt(req.query.page) - 1;
+      const result = await productsCollection
+        .find(query)
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
+    app.get("/productsCount", async (req, res) => {
+      const { name, category, brand } = req.query || "";
+      let query = {};
+
+      if (name || category || brand) {
+        query = {
+          $or: [],
+        };
+
+        if (name) {
+          query.$or.push({ name: { $regex: name, $options: "i" } });
+        }
+        if (category) {
+          query.$or.push({ category: { $regex: category, $options: "i" } });
+        }
+        if (brand) {
+          query.$or.push({ brand: { $regex: brand, $options: "i" } });
+        }
+      }
+
+      const totalProduct = await productsCollection.countDocuments(query);
+      res.send({ totalProduct });
+    });
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -50,9 +94,9 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", async (req, res) => {
-    res.send("server running");
-  });
-  
-  app.listen(port, () => {
-    console.log(`server running on port ${port}`);
-  });
+  res.send("server running");
+});
+
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
